dfc228b47eba9437cf778c91fcec99d543ffb9a2
Zhu-Borui <borui.25@intl.zju.edu.cn>
2026-03-10 12:24:45 +0100

    change my wrong understanding of randint

--- practical4/mystery_code.py
+++ practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: the code draws 11 random numbers between 1 and 9, sums them up, and prints the total.
+# Answer: the code draws 11 random numbers between 1 and 10, sums them up, and prints the total.
 
 # Import libraries
 # randint allows drawing a random number,
@@ -16,10 +16,9 @@ while progress<=10:
 	#It means progress = progress + 1
 	#It indicates that the loop will run 11 times, as progress starts at 0 and increments by 1 until it reaches 10.
 	progress+=1
-	#draw a random number between 1 and 10, and add it to total_rand.
-	#however, 10 is not included, so the random number will be between 1 and 9.
+    #Draw a random number between 1 and 10 (inclusive), and add it to total_rand.	
 	n = randint(1,10)
 	total_rand+=n
-#After the loop, total_rand will contain the sum of 11 random numbers drawn between 1 and 9.
+#After the loop, total_rand will contain the sum of 11 random numbers drawn between 1 and 10.
 print(total_rand)
 
